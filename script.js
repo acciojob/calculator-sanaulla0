07c7d22e8c37defc295f50b9a434f46b82719f8c
sanaulla0 <vadapallisanaulla@gmail.com>
2022-12-21 14:32:29 +0530

    p3

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+let input =document.getElementById("input");
+let process = '';
+function solve(item){
+
+ if(item=='ans'){
+  let result = eval(process);
+  input.value=result;
+  console.log(result);
+
+ }
+
+ else if(item=='clr'){
+  process='';
+   input.value='';
+
+ }
+ else{
+  process+=item;
+  console.log(process);
+  input.value=process;
+
+ }
+
+
+
+
+}
+
